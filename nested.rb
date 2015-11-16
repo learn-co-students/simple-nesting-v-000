c9fba527a7336025f1523966da674da8dd6c2681
Shana L Moore <shana.lavina.moore@gmail.com>
2015-11-15 23:34:49 -0600

    Done.

--- nested.rb
+++ nested.rb
@@ -1,5 +1,7 @@
+require 'pry'
 
 def hopper
+
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -16,6 +18,7 @@ def hopper
         }
      }
 
+  programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +40,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+  programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,6 +61,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -80,6 +88,9 @@ def adding_matz
         }
      }
 
+  programmer_hash[:yukihiro_matsumoto] = {}
+
+  programmer_hash
     
 end
 
@@ -100,9 +111,11 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     # alans_new_info = "GUI"
      
-     
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -122,7 +135,10 @@ def adding_to_dennis
         }
      }
 
-     
+  programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+
+  programmer_hash
+
 end
 
 
