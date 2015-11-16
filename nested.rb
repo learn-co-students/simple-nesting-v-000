1a7eaa5520e523e4b145a5d444a4eefec00dfda9
Shana L Moore <shana.lavina.moore@gmail.com>
2015-11-15 23:38:06 -0600

    Done.

--- nested.rb
+++ nested.rb
@@ -111,9 +111,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for the value of the alans_new_info variable. 
-     # alans_new_info = "GUI"
+     alans_new_info = "GUI"
      
-     programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
 
      programmer_hash
 end
@@ -135,7 +135,7 @@ def adding_to_dennis
         }
      }
 
-  programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 
   programmer_hash
 
