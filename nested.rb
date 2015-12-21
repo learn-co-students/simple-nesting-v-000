8a7ede7e6f81a7934955f2ceef5d990c4ed787d5
phamducv <phamducv@gmail.com>
2015-12-21 20:35:25 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -57,7 +57,8 @@ def dennis_ritchies_language
         }
      }
 
-        return programmer_hash[:dennis_ritchie][:languages][0]
+        return programmer_hash[:dennis_ritchie][:languages].first
+        #or programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
