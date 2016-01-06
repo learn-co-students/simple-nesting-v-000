0fd064edaf2624238705aea1c06ae3173f1c7fb7
pajamaw <pjpeterww@gmail.com>
2016-01-06 16:24:05 -0500

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,14 +57,13 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
 # add the following information to the top level of the programmer hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP, C"]
-# }
+
 
 	programmer_hash = 
  		{
@@ -79,9 +80,13 @@ def adding_matz
           :languages => ["C"]
         }
      }
+programmer_hash[:yukihiro_matsumoto] = {
+ :known_for => "Ruby",
+ :languages => ["LISP, C"]
+}
 
-    
-end
+programmer_hash
+end 
 
 def changing_alan
 	programmer_hash = 
@@ -100,9 +105,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
      
-     
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -121,9 +126,11 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
-     
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
+
+
 
 
 
