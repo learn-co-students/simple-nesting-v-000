960f95cbab59511a879b4424187b47f742d67fc8
srutisriram <srutisriram87@gmail.com>
2016-03-08 16:18:40 -0500

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -77,7 +79,12 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
+        },
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          languages: ["LISP, C"]
         }
+
      }
 
     
@@ -99,9 +106,10 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
      
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+     #change what Alan Kay is :known_for the value of the alans_new_info variable. 
+     programmer_hash
      
 end
 
@@ -121,7 +129,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+    programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
+    programmer_hash
      
 end
 
