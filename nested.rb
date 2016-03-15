dec45f6e83f4945433891881f451c4d6c10fc3ca
ARAF91 <araft50@yahoo.com>
2016-03-15 14:50:28 -0400

    Done.

--- nested.rb
+++ nested.rb
@@ -16,8 +16,9 @@ def hopper
         }
      }
 
-
+ programmer_hash.fetch(:grace_hopper)
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  programmer_hash.fetch(:alan_kay).fetch(:known_for)
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  programmer_hash.fetch(:dennis_ritchie).fetch(:languages)[0]
 end
 
 def adding_matz
@@ -80,7 +83,8 @@ def adding_matz
         }
      }
 
-    
+  programmer_hash.store( :yukihiro_matsumoto, {:known_for => "Ruby", :languages => ["LISP", "C"] } )
+  programmer_hash  
 end
 
 def changing_alan
@@ -101,13 +105,16 @@ def changing_alan
      }
      #change what Alan Kay is :known_for the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+    programmer_hash
+
      
      
 end
 
 def adding_to_dennis
-	programmer_hash = 
- 		{
+  programmer_hash =
+    {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -122,11 +129,10 @@ def adding_to_dennis
         }
      }
 
-     
+ programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
+
 end
-
-
-
 
 
 
