b3eac3ffebee128aaa8a3e820778dec1d38bfc20
cschleif <cschleif@gmail.com>
2016-03-24 21:37:30 -0700

    Done.

--- nested.rb
+++ nested.rb
@@ -55,6 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages].first
 end
 
 def adding_matz
@@ -105,9 +106,10 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for the value of the alans_new_info variable. 
-    programmer_hash[:alan_kay][:known_for] = "GUI"
-     
-     
+    alans_new_info = "GUI"
+
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
+    programmer_hash
 end
 
 def adding_to_dennis
@@ -126,7 +128,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 programmer_hash
      
 end
