c7aa679f35a228a8a848f9d7393849230c19ccca
Sandy Hogan <svern99@aol.com>
2016-05-28 14:38:49 -0400

    Done.

--- nested.rb
+++ nested.rb
@@ -102,7 +102,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for the value of the alans_new_info variable.
      alans_new_info = "GUI"
-		 programmer_hash[:alan_kay] = :known_for => alans_new_info
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+		 programmer_hash
 
 end
 
@@ -122,6 +123,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
-
+		 programmer_hash[:dennis_ritchie][:languages] << ("Assembly")
+		 programmer_hash
 end
