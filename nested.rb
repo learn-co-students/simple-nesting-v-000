affc1949512b617407c8e090bd5eac0ae3e95ff3
aarondevon <me@aarondevon.com>
2016-06-20 12:38:53 -0500

    update #adding_matz method

--- nested.rb
+++ nested.rb
@@ -83,10 +83,7 @@ def adding_matz
         }
      }
 
-     programmer_hash[:yukihiro_matsumoto] = {}
-
-     programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-     programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP, C"]
+     programmer_hash[:yukihiro_matsumoto] = {known_for: "ruby", languages: ["LISP", "C" ]}
 
      return programmer_hash
 end
