0cc38fe27e02bc6a45fef957ea064893f42fcfaf
noson cohen <natancohen23@gmail.com>
2016-10-09 19:32:42 +0000

    nesting

--- nested.rb
+++ nested.rb
@@ -108,7 +108,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for] = "GUI"
-		 return programmer_hash[:alan_kay][:known_for]
+
+		 programmer_hash
 end
 
 def adding_to_dennis
@@ -127,6 +128,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages][1] << "Assembly"
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+		 programmer_hash
 
 end
