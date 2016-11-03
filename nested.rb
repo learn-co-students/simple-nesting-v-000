c2603ac7f02f6daf56116c8dd55d7069855aec5b
Scott Wakeman <swakeman@ca.rr.com>
2016-11-03 19:56:39 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -106,8 +106,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
 
-		 programmer_hash[:alan_kay][:known_for] = ("GUI")
-		 changing_alan
+		 programmer_hash[:alan_kay][:known_for] = "GUI"
+
 		 #programmer_hash[:alan_kay] = [alans_new_info]
 		 #programmer_hash[:alan_kay][alans_new_info] = "GUI"
 
