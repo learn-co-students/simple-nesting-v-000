62de1245d7bff1f30511f666caa72aa43a87e304
Senad Hadrovic <senadhadrovic@gmail.com>
2016-11-21 20:40:53 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -83,6 +83,7 @@ def adding_matz
         }
      }
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP, C"]}
+programmer_hash
 end
 
 
@@ -105,6 +106,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = "GUI"
+		 programmer_hash
 
 end
 
@@ -126,4 +128,5 @@ def adding_to_dennis
      }
 
 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
