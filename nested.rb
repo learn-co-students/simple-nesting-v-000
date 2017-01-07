9d3d3b5be2d984a3044a56a2cf4a8e873d3edd4b
charlie lee <ctlee9096@gmail.com>
2017-01-07 22:39:01 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -112,7 +112,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 programmer_hash[:alan_kay][:known_for]="GUI"
-return programmer_hash[:alan_kay][:known_for]
+return programmer_hash
 end
 
 def adding_to_dennis
@@ -131,5 +131,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages]<<"Asse"
+programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
+binding.pry
+return programmer_hash
 end
