d32f1859af3c857c12dbc3979270c9dd7adf3c1d
Elysa Silveira <elysa.silveira@gmail.com>
2017-05-23 02:29:15 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+     return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,15 +57,10 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP, C"]
-# }
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -80,7 +77,12 @@ def adding_matz
         }
      }
 
-    
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP, C"]
+     }
+
+    return programmer_hash
 end
 
 def changing_alan
@@ -100,8 +102,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
+  programmer_hash[:alan_kay][:known_for] ="GUI"
+  
+  return programmer_hash   
      
 end
 
@@ -121,5 +124,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+
+return programmer_hash
 
 end
