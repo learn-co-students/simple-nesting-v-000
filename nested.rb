361827008d0588991e0967b86998c4200ae9a8ca
Jacqlene Sifre <jsifre01@gmail.com>
2017-05-30 16:09:32 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -108,9 +108,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-		 programmer_hash[:alan_kay][:known_for] = "GUI"
-		 return programmer_hash[:alan_kay][:known_for]
-		 #binding.pry
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+		 return programmer_hash
 	end
 
 def adding_to_dennis
@@ -130,6 +129,6 @@ def adding_to_dennis
         }
      }
 		programmer_hash[:dennis_ritchie][:languages].push("Assembly")
-
+		return programmer_hash
 
 end
