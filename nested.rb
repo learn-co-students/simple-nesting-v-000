6a751e112301f5558b12d399a8eedc13698df334
Trisha Garrett <trisha.garrett@gmail.com>
2017-10-31 00:56:15 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -92,6 +92,7 @@ def adding_matz
 end
 
 def changing_alan
+
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -111,6 +112,8 @@ def changing_alan
 
 	   #change what Alan Kay is :known_for to the value of the alans_new_info variable.
 		 #alans_new_info = "GUI"
+
+		 programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
@@ -130,4 +133,5 @@ def adding_to_dennis
         }
      }
 
+  programmer_hash[:dennis_ritchie][:languages]["C"] << "Assembly"
 end
