38125b7b3022eaa22df5f9a102216fee59815ab8
jtorre20 <jtorres1989@gmail.com>
2017-12-13 17:36:40 -0500

    done

--- nested.rb
+++ nested.rb
@@ -87,13 +87,7 @@ def adding_matz
      programmer_hash[:yukihiro_matsumoto] = {:known_for=>"RUBY", :languages => ["LISP", "C"]}
      programmer_hash
 end    
-      #programmer_hash[:yukihiro_matsumoto] = {:known_for => "RUBY", :languages =>}
-      #programmer_hash[yukihiro_matsumoto]  :known_for]
-      #programmer_hash[:yukihiro_matsumoto][:known_for] = "RUBY"
-      #programmer_hash[:yukihiro_matsumoto][:known_for][:languages] = ["LISP", "C"]  
-      
-
-
+     
 def changing_alan
 	programmer_hash = 
  		{
@@ -112,6 +106,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash
      
      
 end
@@ -132,5 +128,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
