48d92720a0a390abe6b9d61edd5842671f5ebeec
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-13 18:32:43 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -14,8 +14,9 @@ def hopper
           :known_for => "Unix",
           :languages => ["C"]
         }
+       
      }
-
+ programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     string = programmer_hash[:dennis_ritchie][:languages]
+     string.join
 end
 
 def adding_matz
@@ -79,7 +83,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto] = {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]}
+  
+  programmer_hash
     
 end
 
@@ -99,8 +107,10 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+     
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
      
 end
@@ -121,5 +131,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
