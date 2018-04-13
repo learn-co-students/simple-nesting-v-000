cd1a5c1d1d6dcfaecdf909e1c73ad91af58a68cf
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-04-13 21:33:32 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -103,10 +103,8 @@ def changing_alan
         }
      }
      alans_new_info = "GUI"
-
-		 programmer_hash[:alan_kay][:known_for] = "GUI"
-
-		 programmer_hash[:alan_kay][:known_for]
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+		 programmer_hash
 
 end
 
@@ -126,6 +124,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+	programmer_hash
 
 end
