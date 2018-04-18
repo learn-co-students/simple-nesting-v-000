2a34999e33345f47c6c73b13225404d9776cc156
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-04-18 02:24:43 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -131,5 +131,6 @@ def adding_to_dennis
         }
      }
 
-programmer_hash[:dennis_ritchie][:languages].push "Assembly" # << or [1] works too but push is pretty
+programmer_hash[:dennis_ritchie][:languages].push("Assembly") # << or [1] works too but push is pretty
+programmer_hash
 end
