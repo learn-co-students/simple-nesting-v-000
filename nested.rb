e8ff6c2f0727e35ac01ae48d0187b3dab14cc035
ERIC KELLY <eakelly220@gmail.com>
2018-04-18 22:26:11 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,5 @@
 
+require 'pry'
 def hopper
 	programmer_hash = 
  		{
@@ -15,7 +16,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -62,7 +65,7 @@ def adding_matz
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
-# }
+# }https://learn.co/tracks/full-stack-web-development-v5/procedural-ruby/data-structures/multitype-collections-lab?batch_id=306&track_id=31303#
 
 	programmer_hash = 
  		{
@@ -79,9 +82,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
-end
+  programmer_hash  [:yukihiro_matsumoto] = {yukihiro_matsumoto: {known_for: "Ruby", languages: ["Lisp","C"]}}
+  end
 
 def changing_alan
 	programmer_hash = 
@@ -101,6 +103,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
+     
      
      
 end
@@ -121,5 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+  programmer_hash
 end
