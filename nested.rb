1787b017d503df2d783bc67aef252bfa1e587045
Michael Blandford <mdblandford@gmail.com>
2018-04-23 20:13:11 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,5 +1,6 @@
 
 def hopper
+  # operates on the programmer_hash and returns the value of the :grace_hopper key
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -15,13 +16,11 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
+	# operates on the programmer_hash and returns the value of what Alan Kay is known for
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,9 +36,11 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
+  # operates on the programmer_hash and returns the value of Dennis Ritchie's language as a string
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -63,7 +65,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
+#operates on the programmer_hash and adds a key/value pair to the top level of the hash, returningthe newly-added-to hash
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -79,11 +81,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+  programmer_hash.merge(:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]})
 end
 
 def changing_alan
+	# operates on the programmer_hash and changes what Alan Kay is known for, returning the newly-changed hash
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -101,8 +103,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +123,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
 end
