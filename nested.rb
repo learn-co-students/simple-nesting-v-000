e368fc27339ccf8324ea6610471324cae19d3f0f
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-05-21 15:21:36 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -61,16 +61,12 @@ def dennis_ritchies_language
         }
      }
 
-programmer_hash[:dennis_ritchie][:languages]
+languages = programmer_hash[:dennis_ritchie][:languages]
 languages[0]
 
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# dennis_ritchies_language
-	programmer_hash
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -86,7 +82,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+     
+programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["Lisp", "C"]}
 programmer_hash
 
 end
@@ -108,9 +105,10 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
-     
+
+programmer_hash[:alan_kay][:known_for] ="GUI"
+programmer_hash
+
 end
 
 def adding_to_dennis
@@ -129,5 +127,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
