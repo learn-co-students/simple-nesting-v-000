2b0da975323697d24d1276757b1d28d7700adf2e
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-05-21 12:32:36 +0000

    Old code that is not working

--- nested.rb
+++ nested.rb
@@ -16,12 +16,14 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
 end
 
+
+
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
+ 
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +39,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,14 +60,16 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+programmer_hash[:dennis_ritchie][:languages]
+languages[0]
+
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+# dennis_ritchies_language
+	programmer_hash
 
 	programmer_hash = 
  		{
@@ -80,7 +87,8 @@ def adding_matz
         }
      }
 
-    
+programmer_hash
+
 end
 
 def changing_alan
