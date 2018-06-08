2592cc9701d904b4e4bf94fbe81ae197beff0d6b
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-06-08 14:48:27 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 def hopper
 	programmer_hash = 
@@ -15,8 +16,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+     programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
