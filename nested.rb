ac88ed8f7126cdfabc2d744dbec68a5ba497e6ea
Amit Singh <amit_nyc@live.com>
2018-06-10 02:29:47 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 return programmer_hash[:dennis_ritchie][:languages]
+		 return programmer_hash[:dennis_ritchie][:languages].join(" ")
 end
 
 def adding_matz
@@ -82,6 +82,10 @@ def adding_matz
         }
      }
 
+programmer_hash[:yukihiro_matsumoto] = [:known_for] << "Ruby"
+programmer_hash[:yukihiro_matsumoto] = [:languages]<< ["LISP", "C"]
+
+return programmer_hash
 
 end
 
@@ -103,7 +107,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
+programmer_hash[:alan_kay][:known_for] = "GUI"
+return programmer_hash
 
 end
 
@@ -123,5 +128,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+return programmer_hash
 end
