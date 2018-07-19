9d3038192c6f3f9ee81a7d1b5117ec09f8497bc0
Niusha H <niusha.hadziomerovic@gmail.com>
2018-07-19 05:56:55 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -140,6 +140,10 @@ def adding_to_dennis
 
 programmer_hash[:dennis_ritchie][:languages].push "Assembly"
 
+# Or, you could add the string like this, too:
+# programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+
 return programmer_hash
 
 end
