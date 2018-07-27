3a6943d2e13a6d8b8981f5d7b550a886518e0f9a
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-07-27 16:32:48 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
-
+    
+    return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,14 +57,11 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+    return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
 
 	programmer_hash = 
  		{
@@ -77,11 +76,18 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        }
-     }
-
-    
+        } 
+ 		
+ 		}
+ 		
+ 		programmer_hash[:yukihiro_matsumoto] = {}
+ 	  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+    programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"] 
+ 		  
+  return programmer_hash
 end
+
+
 
 def changing_alan
 	programmer_hash = 
@@ -121,5 +127,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
 
 end
