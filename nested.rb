7d2b909d1b4dad51acae6d2638ce6a53a1009862
Heshie Brody <4hbrody@gmail.com>
2018-08-16 02:25:10 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 def hopper
 	programmer_hash =
  		{
@@ -81,8 +81,10 @@ def adding_matz
         }
      }
 
-
+     programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+		 return programmer_hash
 end
+
 
 def changing_alan
 	programmer_hash =
@@ -102,8 +104,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
-
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+		 return programmer_hash
 end
 
 def adding_to_dennis
@@ -122,5 +124,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+		 programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+		 return programmer_hash
 end
