f792f1367598785a4b455494a146147157157156
Cheryl K <cherylkelson@icloud.com>
2018-09-22 19:25:58 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages].first
+
 end
 
 def adding_matz
@@ -80,7 +84,8 @@ def adding_matz
         }
      }
 
-    
+   programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+    programmer_hash
 end
 
 def changing_alan
@@ -103,6 +108,9 @@ def changing_alan
      alans_new_info = "GUI"
      
      
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
+    programmer_hash
+
 end
 
 def adding_to_dennis
@@ -121,5 +129,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 
 end
