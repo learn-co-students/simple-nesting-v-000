ff586974a4d400973af585c41910d676ea17d23e
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-10-08 18:58:35 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,3 @@
-
 def hopper
 	programmer_hash = 
  		{
@@ -15,7 +14,8 @@ def hopper
           :languages => ["C"]
         }
      }
-
+  
+  programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,7 +82,14 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+     
+     programmer_hash = 
+     {
+      :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]     
+      }
+     }
     
 end
 
@@ -100,9 +110,11 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
-     
+    alans_new_info = "GUI"
+    alan = programmer_hash[:alan_kay]
+    alan[:known_for] = alans_new_info
+    programmer_hash
+    
 end
 
 def adding_to_dennis
@@ -121,5 +133,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     dennis = programmer_hash[:dennis_ritchie][:languages]
+     dennis << "Assembly"
+     programmer_hash
 
 end
