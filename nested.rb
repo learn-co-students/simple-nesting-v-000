26835060793814595da432d26c642eaa7f9c8d14
Arnold Su <arnoldtingsu@gmail.com>
2018-11-18 09:15:19 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,11 +16,10 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
  		{
@@ -37,7 +36,12 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+programmer_hash[:alan_kay][:known_for]
+
 end
+
+
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -55,6 +59,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+      programmer_hash[:dennis_ritchie][:languages].shift
+     
 end
 
 def adding_matz
@@ -80,7 +87,9 @@ def adding_matz
         }
      }
 
+ programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :language => ["LISP", "C"]}
     
+    programmer_hash
 end
 
 def changing_alan
@@ -102,6 +111,10 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     
+     programmer_hash
+     
      
 end
 
@@ -121,5 +134,17 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     
+     
+     
+     
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+programmer_hash
+
+
 
 end
+
+
